c254e39bc7c228c4ff507f0c30cb3206ee0b7fa0
Kunal Bhat <kunal.bhat2001@gmail.com>
2022-12-13 00:50:56 +0530

    Push refs

--- tests/ghost_bench.cc
+++ tests/ghost_bench.cc
@@ -3,21 +3,26 @@
 #include "lib/base.h"
 #include "lib/ghost.h"
 #include <cstdlib>
+#include <string>
 
-void TestFunction()
+void TestFunction(std::string cmd = "")
 {
     std::cout << "Hello world! Starting a ghost thread\n";
-    ghost::GhostThread t(ghost::GhostThread::KernelScheduler::kGhost, []
+    const char * c_cmd = cmd.c_str();
+    ghost::GhostThread t(ghost::GhostThread::KernelScheduler::kCfs, [c_cmd]
                          {
         std::cout << "In Ghost\n";
         struct sched_param param = {};
-        sched_setscheduler(0, SCHED_OTHER, &param);
-        std::system("perf stat -e cs sysbench threads --threads=256 run");
+        sched_setscheduler(0, SCHED_ISO, &param);
+        //std::system("sysbench threads --threads=256 run");
+        std::system(c_cmd);
     });
     t.Join();
 }
 
-int main()
+int main(int argc, char * argv[])
 {
-    TestFunction();
+    std::string cmd = "sysbench threads run --threads=";
+    cmd += argv[1];
+    TestFunction(cmd);
 }
